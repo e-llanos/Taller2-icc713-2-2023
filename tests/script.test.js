012ca26b3288dac3ba41e9acb22311de0cedbf24
Esteban Llanos <e.llanos01@ufromail.cl>
2023-10-17 23:50:37 -0300

    completed tests

--- tests/script.test.js
+++ tests/script.test.js
@@ -1,5 +1,11 @@
 const { expect } = require('@jest/globals');
-const { getTwoByConsole } = require('../src/script');
+const { 
+  getTwoByConsole,
+  getTwoByGenre,
+  getGameByConsoleAndGenre,
+  getGameByName,
+  getGamesByGenre,
+ } = require('../src/script');
 
 describe('Script tests', () => {
   describe('getTwoByConsole', () => {
@@ -22,43 +28,72 @@ describe('Script tests', () => {
 
   describe('getTwoByGenre', () => {
     it('Should return two games by Survival Horror genre', () => {
-      expect(0).toBeTruthy();
+      const games = getTwoByGenre('Survival Horror');
+      expect(games).toHaveLength(2);
+      for (const game of games) {
+        expect(game.genres).toContain('Survival Horror');
+      }
     });
     it('Should throw and error when the console doesnt exists', () => {
-      expect(0).toBeTruthy();
+      try {
+        getTwoByGenre('GENRE');
+        throw new Error('other-error');
+      } catch (error) {
+        expect(error.message).not.toEqual('other-error');
+      }
     });
   });
 
   describe('getGameByConsoleAndGenre', () => {
     it('Should return a game for GBA and Sport genre', () => {
-      expect(0).toBeTruthy();
+      const game = getGameByConsoleAndGenre('GBA', 'Sport');
+      expect(game).not.toBeNull();
+      expect(game.video_console).toEqual('GBA');
+      expect(game.genres).toEqual('Sport');
     });
 
     it('Should throw and error when the console doesnt exists', () => {
-      expect(0).toBeTruthy();
+      try {
+        getGameByConsoleAndGenre('CONSOLE', 'Sport');
+        throw new Error('other-error');
+      } catch (error) {
+        expect(error.message).not.toEqual('other-error');
+      }
     });
 
     it('Should throw and error when the genre doesnt exists', () => {
-      expect(0).toBeTruthy();
+      try {
+        getGameByConsoleAndGenre('GBA', 'GENRE');
+        throw new Error('other-error');
+      } catch (error) {
+        expect(error.message).not.toEqual('other-error');
+      }
     });
   });
 
   describe('getGameByName', () => {
     it('Should return the Resident evil 2 game', () => {
-      expect(0).toBeTruthy();
+      const game = getGameByName('Resident Evil 2');
+      expect(game).not.toBeNull();
+      expect(game.name).toEqual('Resident Evil 2');
     });
 
     it('Should return undefined when not found', () => {
-      expect(0).toBeTruthy();
+      const game = getGameByName('Non-Existent');
+      expect(game).toBeUndefined();
     });
   });
 
   describe('getGamesByGenre', () => {
     it('Should return a list of games by Action genre', () => {
-      expect(0).toBeTruthy();
+      const games = getGamesByGenre('Action');
+      for (const game of games) {
+        expect(game.genres).toContain('Action');
+      }
     });
     it('Should return a empty list when the genre doesnt exists', () => {
-      expect(0).toBeTruthy();
+      const games = getGamesByGenre('Non-Existent');
+      expect(games).toHaveLength(0);
     });
   });
 });
